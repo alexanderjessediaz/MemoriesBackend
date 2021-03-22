f23a0e4219c43265a7ee9aca67296dd8ae2c0598
AJ <alexanderjessediaz@gmail.com>
2021-03-22 11:52:05 -0600

    add model, controller, and route files. Create postMessage Schema layout, set up getPosts and createPost functions

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(express.json({limit: "20mb", extended: true}));
+app.use(express.urlencoded({limit: "20mb", extended: true}));
+
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://jackiediaz:jackiediaz123@cluster0.gwwmq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => app.listen(PORT, () =>
+console.log(`Connection is established and running on port:${PORT}`)
+)).catch((err)=> console.log(err.message));
+
+mongoose.set("useFindAndModify", false);
