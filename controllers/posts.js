fadee814344fd806afbbd4633711a26f56f9e758
AJ <alexanderjessediaz@gmail.com>
2021-03-23 17:13:38 -0600

    import moongose and change spelling of Object(s)ID to ObjectId in controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,5 @@
-
+// import express from 'express';
+import mongoose from 'mongoose';
 import PostMessage from '../models/postsMessage.js';
 
 export const getPosts = async (req, res) => {
@@ -26,12 +27,12 @@ export const createPost = async (req, res) => {
 }
 
 export const updatePost = async (req,res) => {
-    const { id: _id } = req.params;
+    const  { id: _id } = req.params;
     const post = req.body;
 
-    if(!mongoose.Types.ObjectsID.isValid(_id)) return res.status(404).send('No post with that id');
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
-    const updatedPost= await PostMessage.findByIdAndUpdate(_id, post, { new: true});
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true});
 
     res.json(updatedPost);
 }
