3d62a95612ff190ffdbafc0d01f427f6c62310c3
AJ <alexanderjessediaz@gmail.com>
2021-03-24 14:01:30 -0600

    add dotenv for db auth

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 
 app.use(express.json({limit: "20mb", extended: true}));
 app.use(express.urlencoded({limit: "20mb", extended: true}));
@@ -15,11 +16,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://jackiediaz:jackiediaz123@cluster0.gwwmq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {
+mongoose.connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => app.listen(PORT, () =>
